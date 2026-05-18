35442fbd6c1ac9ef67db441940e3963452ed956f
Saadi bechir <saadibechiring@gmail.com>
2026-05-18 14:26:30 +0200

    add user 2

--- src/app/core/auth.interceptor.ts
+++ src/app/core/auth.interceptor.ts
@@ -1,12 +1,37 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpErrorResponse,
+  HttpInterceptorFn
+} from '@angular/common/http';
+
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const router = inject(Router);
+
   const token = localStorage.getItem('token');
-  if (!token || req.url.includes('/api/auth/login')) {
-    return next(req);
-  }
-  const authReq = req.clone({
-    setHeaders: { Authorization: `Bearer ${token}` }
-  });
-  return next(authReq);
+
+  const authReq = token
+    ? req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+    : req;
+
+  return next(authReq).pipe(
+    catchError((error: HttpErrorResponse) => {
+
+      if (error.status === 401 || error.status === 403) {
+
+        localStorage.removeItem('token');
+
+        router.navigate(['/login']);
+      }
+
+      return throwError(() => error);
+    })
+  );
 };
